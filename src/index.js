799eb9bc7e546d62bf1753519d4bff300592145e
ganesh-91 <ganesh.khaire91@gmail.com>
2017-08-23 22:36:43 +0530

    added basic development files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+class ReactFormBuilder extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {}
+}
+
+export default ReactFormBuilder;
