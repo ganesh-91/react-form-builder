7dae23610887e5bbe05ba3fee90074b607e3829b
ganesh-91 <ganesh.khaire91@gmail.com>
2017-08-25 19:21:18 +0530

    webpack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,60 @@
-var path = require("path");
-module.exports = {
-    entry: "./src/index.js",
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const WebpackConfig = {
+
+    entry: APP_DIR + '/index.js',
+
     output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "index.js",
-        libraryTarget: "commonjs2" // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+        path: BUILD_DIR,
+        filename: 'index.js',
+        libraryTarget: "commonjs2"
     },
+
     module: {
         rules: [
             {
+                loader: 'babel-loader',
                 test: /\.js$/,
-                include: path.resolve(__dirname, "src"),
-                exclude: /(node_modules|bower_components|build)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["env"]
-                    }
+                exclude: /node_modules/,
+                include: APP_DIR,
+                options: {
+                    presets: ['env', 'react']
                 }
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
             }
-        ]
+        ],
     },
-    externals: {
-        "react": "commonjs react" // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-    }
-};
+
+}
+
+
+// webpack production config.
+if (process.env.NODE_ENV === 'production') {
+
+    WebpackConfig.externals = {
+        'react': 'react',
+        'react-dom': 'react-dom'
+    };
+
+    WebpackConfig.plugins = [
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+            },
+            compress: {
+                warnings: false,
+                screw_ie8: true
+            },
+            comments: false
+        }),
+    ];
+
+}
+
+
+module.exports = WebpackConfig;
