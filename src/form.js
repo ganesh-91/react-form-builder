b9af270234ac6ecd3d0448bdf47a436621b25658
ganesh-91 <ganesh.khaire91@gmail.com>
2017-08-25 20:40:43 +0530

    basic poc implemented

---
+++ src/form.js
@@ -0,0 +1,49 @@
+const formJson = {
+    name: "name",
+    id: "1",
+    formFeilds: [
+        {
+            value: "",
+            type: "text",
+            name: "input",
+            tagName: "input",
+            id: "",
+            placeHolder: "",
+            disabled: false
+        },
+        {
+            value: "",
+            type: "button",
+            name: "input",
+            tagName: "input",
+            id: "",
+            placeHolder: "",
+            disabled: false
+        },
+        {
+            value: "",
+            type: "",
+            name: "",
+            tagName: "select",
+            id: "",
+            placeHolder: "",
+            disabled: false,
+            options: [
+                {
+                    value: "1",
+                    label: "1"
+                },
+                {
+                    value: "2",
+                    label: "2"
+                },
+                {
+                    value: "3",
+                    label: "3"
+                }
+            ]
+        }
+    ]
+};
+
+export default formJson;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import React from "react";
 import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class ReactFormBuilder extends React.Component {
     constructor() {
         super();
         this.state = {};
     }
-    render() {}
+    render() {
+        console.log("formJson", this.props.formJson);
+        const formJson = this.props.formJson;
+        return (
+            <form id={formJson.id || ""} className={formJson.className || ""} name={formJson.name || ""}>
+                {formJson.formFeilds.map((feild) => {
+                    switch (feild.tagName) {
+                        case "input":
+                            return (<input value={feild.value || ""}
+                                type={feild.type}
+                                placeholder={feild.placeHolder || ""}
+                                name={feild.name || ""} />);
+                            break;
+
+                        case "select":
+                            return (<select value={feild.value || ""}
+                                placeholder={feild.placeHolder || ""}
+                                name={feild.name || ""} >
+                                {feild.options.map((option) => {
+                                    return (<option value={option.value}>{option.label}</option>);
+                                })}
+                            </select >);
+                            break;
+                    }
+                    {/* return (<{ feild.tagName } />); */ }
+                })}
+            </form>
+        );
+    }
 }
 
 export default ReactFormBuilder;
